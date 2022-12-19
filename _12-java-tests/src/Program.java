52697dae2730cefd51301519b5265acd4a34f2f6
Alina <wbrflf3301@outlook.com>
2022-12-19 09:39:01 +0500

    eststgsetse

--- _12-java-tests/src/Program.java
+++ _12-java-tests/src/Program.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         String l = String.valueOf(Integer.MIN_VALUE);
-        System.out.print(Program.Decode("015"));
+        System.out.print(Program.Decode("256-"));
     }
     public static Integer Decode(String nm){
         return Integer.decode(nm);
--- _12-java-tests/src/Tests.java
+++ _12-java-tests/src/Tests.java
@@ -1,58 +1,58 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
 
 public class Tests {
 
     @Test(expected = NumberFormatException.class)
-    public void Empty() throws Exception {
+    public void Empty() {
         Program.Decode("");
     }
 
     @Test
-    public void FirstPlus() {
+    public void FirstPlus(){
         assertEquals(Integer.valueOf(14), Program.Decode("+14"));
     }
 
     @Test
-    public void FirstMinus() {
+    public void FirstMinus(){
         assertEquals(Integer.valueOf(-14), Program.Decode("-14"));
     }
 
     @Test(expected = NumberFormatException.class)
-    public void SecondMinus() {
-        Program.Decode("2-");
-    }
-
-    @Test(expected = NumberFormatException.class)
-    public void SecondPlus() {
-        Program.Decode("2+");
+    public void TwoMinus(){
+        Program.Decode("--2");
     }
 
     @Test
-    public void StartsWithNullX() {
+    public void NegativeNumber(){
+        assertEquals(Integer.valueOf(-2), Program.Decode("-2"));
+    }
+
+    @Test
+    public void StartsWithNullX(){
         assertEquals(Integer.valueOf(18), Program.Decode("0x12"));
     }
 
     @Test
-    public void StartsWithSharp() {
+    public void StartsWithSharp(){
         assertEquals(Integer.valueOf(18), Program.Decode("#12"));
     }
 
     @Test
-    public void StartsWithNull() {
+    public void StartsWithNull(){
         assertEquals(Integer.valueOf(13), Program.Decode("015"));
     }
 
     @Test(expected = NumberFormatException.class)
-    public void StrangeThings() {
+    public void StrangeThings(){
         Program.Decode("00ss");
     }
 
     @Test
-    public void IntMinValue() {
+    public void IntMinValue(){
         String l = String.valueOf(Integer.MIN_VALUE);
-        assert (Integer.MIN_VALUE == Program.Decode(l));
+        assert(Integer.MIN_VALUE == Program.Decode(l));
     }
 }
